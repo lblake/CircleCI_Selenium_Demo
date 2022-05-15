6daf03e345241a610f38483a65b5dba1aa8e6965
BharathShrikanth <30857865+BharathShrikanth@users.noreply.github.com>
2022-05-16 00:23:02 +0530

    importing chromeoptions

--- src/main/java/tests/NewTest.java
+++ src/main/java/tests/NewTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
